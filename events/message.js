dc8a3fe773f313f38203f57a586367ef2cc0d5a7
Christain Williamson <Aydenlow06@outlook.com>
2021-03-19 15:45:48 +0000

    New Bot

--- events/message.js
+++ events/message.js
@@ -1,24 +1,53 @@
-module.exports = async (client, message) => {
-  if (message.author.bot) return;
 
-  const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
-  
-  const prefix = message.content.match(prefixMention) ? message.content.match(prefixMention)[0] : client.config.prefix;
-
-  if (message.content.indexOf(prefix) !== 0) return;
-
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  
-  const command = args.shift().toLowerCase();
-
-  const cmd = client.commands.get(command);
-  
-  const aliases = client.commands.find(x => x.info.aliases.includes(command))
+const { Client, Message, MessageEmbed } = require('discord.js');
+/**
+ * 
+ * @param {Client} client 
+ * @param {Message} message 
+ */
 
 
-  if(cmd){
-    cmd.run(client, message, args);
-  }else if(aliases){
-    aliases.run(client, message, args);
-  }else return
-};
+module.exports = async(client, message) => {
+    if (message.author.bot) return;
+
+    var prePrefix;
+    if (require('../database/prefixes.json')[message.guild.id]) {
+        prePrefix = await require('../database/prefixes.json')[message.guild.id].prefix;
+
+    } else {
+        prePrefix = mainPrefix
+    }
+    const escapeRegex = require('../utils/structure/exports/escapeRegex').escapeRegex
+    const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prePrefix)})\\s*`);
+	if (!prefixRegex.test(message.content)) return;
+
+    const [, prefix] = message.content.match(prefixRegex);
+
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+
+    if (!require('../database/modlogs.json')[message.guild.id]) return;
+    let modLogsID  = require('../database/modlogs.json')[message.guild.id].channel;
+
+
+    const cmd = args.shift().toLocaleLowerCase()
+
+    let commandFile = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd))
+
+    if (commandFile) {
+        try {
+    const modLogsEmbed = new MessageEmbed()
+    .setColor(client.color)
+    .setAuthor(message.author.tag, message.author.displayAvatarURL())
+    .setDescription(`Used \`${cmd}\` command in <#${message.channel.id}> \n ${message.content}`)
+    .setFooter(`Author: ${message.author.id} | Message ID: ${message.id}`)
+    .setTimestamp()
+
+
+        
+    await message.guild.channels.cache.get(modLogsID).send(modLogsEmbed);
+        } catch {
+            
+        }
+}
+}
